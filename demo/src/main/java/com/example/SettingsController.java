dafef134630b9ea6f4f5a55e8bd794a616ea0b58
TheRealAJ2002 <ajdemaesschalck@gmail.com>
2023-06-15 16:01:31 +0200

    vertalen toegevoegd

--- demo/src/main/java/com/example/SettingsController.java
+++ demo/src/main/java/com/example/SettingsController.java
@@ -1,7 +1,10 @@
 package com.example;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.TextField;
@@ -9,15 +12,32 @@ import javafx.scene.control.TextField;
 import java.io.IOException;
 
 public class SettingsController {
+
     @FXML
-    private ComboBox<String> languageDropdown;
+    private Button LogOutButton;
 
     @FXML
     private ToggleButton themeToggle;
 
+
+    @FXML
+    private ComboBox<String> languageDropdown;
+    private ObservableList<String> languageOptions = FXCollections.observableArrayList("English", "Dutch", "German");
+
+
     @FXML
     private void initialize() {
-        languageDropdown.getItems().addAll("Nederlands", "Engels", "Duits");
+        Settings settings = new Settings();
+        languageDropdown.setItems(languageOptions);
+
+        // Set the initial selected language based on the ChatController's selectedLanguage
+        languageDropdown.setValue(settings.getSelectedLanguage());
+
+        languageDropdown.setOnAction(event -> {
+            // Get the selected language
+            String selectedLanguage = languageDropdown.getValue();
+            settings.setSelectedLanguage(selectedLanguage);
+        });
 
         themeToggle.setOnAction(event -> {
             Scene scene = themeToggle.getScene();
@@ -36,5 +56,10 @@ public class SettingsController {
     @FXML
     private void goBack() throws IOException {
         Main.changeScene("Chat.fxml");
+    }
+
+    @FXML
+    private void logOut() throws IOException {
+        Main.changeScene("Login.fxml");
     }
 }
