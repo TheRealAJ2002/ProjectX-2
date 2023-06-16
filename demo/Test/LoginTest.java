5d1b8b43a5cb799966c9589d3ecb850830878854
Reno Welleman <R.P.J.Welleman@student.tudelft.nl>
2023-06-16 20:03:14 +0200

    Added test voor nonExistingLogin

--- demo/Test/LoginTest.java
+++ demo/Test/LoginTest.java
@@ -3,14 +3,22 @@ import com.example.AccountManager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class LoginTest() {
+public class LoginTest {
 
     @Test
-    private void testExistingLogin() {
+    public void testExistingLogin() {
         Account existingAccount = new Account("testAccount", "welkom123456");
         AccountManager accManager = new AccountManager();
         accManager.addAccount(existingAccount);
 
-        assertTrue(accManager.accountExists("testAccount", accManager));
+        Assertions.assertTrue(accManager.accountExists("testAccount", accManager));
+    }
+
+    @Test
+    public void testNonExistingLogin() {
+        Account nonExistingAccount = new Account("testAccount", "welkom123456");
+        AccountManager accountManager = new AccountManager();
+
+        Assertions.assertFalse(accountManager.accountExists("testAccount", accountManager));
     }
 }
