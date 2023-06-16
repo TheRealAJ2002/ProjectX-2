53c3ea72c4c3960847acdfdac208259d9e20344e
Reno Welleman <R.P.J.Welleman@student.tudelft.nl>
2023-06-16 20:08:49 +0200

    Account tests toegevoegd

---
+++ demo/Test/AccountTest.java
@@ -0,0 +1,25 @@
+import com.example.Account;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AccountTest {
+    private Account testAccount;
+
+
+    @BeforeEach
+    public void setup() {
+        testAccount = new Account("testNaam", "testWachtwoord");
+    }
+
+    @Test
+    public void setUsernaamTest() {
+        testAccount.setUsername("nieuwNaam");
+        Assertions.assertEquals("nieuwNaam", testAccount.getUsername());
+    }
+
+    @Test
+    public void setPasswordTest() {
+        testAccount.setPassword("nieuwWachtwoord");
+        Assertions.assertEquals("nieuwWachtwoord", testAccount.getPassword());
+    }
+}
